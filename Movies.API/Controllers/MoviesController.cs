718fce5bef713e9b5f89a99a2f6963f82e9f210d
Unicorn-isbad <69872616+Unicorn-isbad@users.noreply.github.com>
2022-05-17 18:00:32 +0200

    Add project files.

---
+++ Movies.API/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Data.Interfaces;
+using Movies.Data.Models;
+
+
+namespace Movies.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepo;
+        public MoviesController(IMovieRepository movieRepository)
+        { 
+            _movieRepo = movieRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> GetMovies()
+        {
+            try
+            {
+                return Ok(_movieRepo.GetAll());
+            }
+            catch (Exception error)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, error.Message);
+            }
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Movie> GetMovieById(int id)
+        {
+            try
+            {
+                var movie = _movieRepo.GetMovieById(id);
+                if (movie == null)
+                {
+                    return NotFound(new { msg = "Rezultat nije pronađen!" });
+                }
+                else return Ok(movie);
+            }
+            catch (Exception error)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { msg = "Nije moguće prikazati rezultate, dogodila se greška!" });
+            }
+        }
+    }
+}
---
+++ Movies.Data/Repositories/MovieRepository.cs
@@ -0,0 +1,44 @@
+﻿using Movies.Data.Interfaces;
+using Movies.Data.Models;
+
+namespace Movies.Data.Repositories
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly algebramssqlhost_moviesContext _context;
+        public MovieRepository(algebramssqlhost_moviesContext context)
+        { 
+            _context = context;
+        }
+
+        public Movie DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Movie> GetAll()
+        {
+            return _context.Movies.ToList();
+        }
+
+        public Movie GetMovieById(int id)
+        {
+            return _context.Movies.Find(id);
+        }
+
+        public Movie InsertMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Movie> QuerryStringFilter(string s, string orderby, int per_page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Movie UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
